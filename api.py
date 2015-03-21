0e9ffe022113fc47182795c9ecda00e37c4e4bc4
Arnaud Loonstra <arnaud@sphaero.org>
2015-03-21 19:24:28 +0000

    added three.js (test) added api class

--- api.py
+++ api.py
@@ -2,14 +2,23 @@
 import sys
 sys.path.append('py')
 import appie
+import webob
+import json
+
+class AmazingApi(appie.AppieRestObject):
+    
+    def handle_GET(self, req, *args, **kwargs):
+        return webob.Response(json.dumps({'test': "test"}), content_type="application/json")
+
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    AppieFS = appie.AppieSimpleFileServer(".", enable_del=True)
+    AppieFS = appie.AppieSimpleFileServer("html", enable_del=True)
+    amaze = AmazingApi()
     appie_inst = appie.Appie(default=AppieFS)
+    appie_inst.register_rest_object("api", amaze)
     # start a webserver
     httpd = make_server('', 8000, appie_inst)
     print("Serving on port 8000...")
     # Serve until process is killed
     httpd.serve_forever()
-
