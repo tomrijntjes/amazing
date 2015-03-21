54e4cfc0b4c54d1a6204b0705abcaaaa1ce16551
tomrijntjes <tomrijntjes@gmail.com>
2015-03-21 17:00:26 +0100

    sniffer

---
+++ sniffer1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from scapy.all import *
+
+stars = lambda n: "*" * n
+
+def GET_print(packet):
+    return "\n".join((
+        stars(40) + "GET PACKET" + stars(40),
+        "\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n")),
+        stars(90)))
+
+sniff(
+    iface='eth0',
+    prn=GET_print,
+    lfilter=lambda p: "GET" in str(p),
+    filter="tcp port 80")
