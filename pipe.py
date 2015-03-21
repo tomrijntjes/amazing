200b153a683c334589f811800f537e3d5411384c
tomrijntjes <tomrijntjes@gmail.com>
2015-03-21 22:56:30 +0100

    pipe turft hosts

--- pipe.py
+++ pipe.py
@@ -2,31 +2,40 @@
 from scapy.all import *
 from scapy.layers import http
 
-from Queue import Queue
-from threading import Thread
-
 stars = lambda n: "*" * n
-q = Queue()
 
+q = list()
+hostdict = {}
+last_referer = "empty string that is not none"
 
-def worker():
-    while True:
-        pkt = q.get()
-        #do something
-        q.task_done()
-
+def packethandler(pkt):
+    global hostdict
+    global last_referer
+    host,referer = None,None
+    for header in str(pkt).splitlines():
+	if "Host" in header:
+	    host = header[6:]
+ 	    #print "Host: " + host
+	if "Referer"  in header:
+	    parts = header[9:].split('//', 1)
+	    referer = parts[0]+'//'+parts[1].split('/', 1)[0]
+	    #print "Referer: " + referer
+    if referer and referer in last_referer:
+	if host in hostdict:
+	    hostdict[host] += 1
+	else:
+	    hostdict[host] = 1
+    if referer and referer not in last_referer:
+	q.append({"referer":last_referer,"hosts":hostdict})
+	last_referer = referer
         
 def main():
-    t = Thread(target=worker)
-    t.daemon = True
-    t.start()
     print("sniffing...")
     sniff(
         iface='eth0',
-        prn=q.put(packet),
+        prn= packethandler,
         lfilter=lambda p: "GET" in str(p),
         filter="tcp port 80")
-    q.join()
 
 
 main()
